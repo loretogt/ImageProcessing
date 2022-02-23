3e4c727e69571f7a342c09ecef8a4c7a5becc804
Loreto Garcia <loreto.garcia.tejada@alumnos.upm.es>
2022-02-23 20:07:58 +0100

    Exercise2

--- Ejercicios1/Exercises_02ab/exercise_02a_thresh.py
+++ Ejercicios1/Exercises_02ab/exercise_02a_thresh.py
@@ -2,14 +2,28 @@
 import cv2
 import sys
 
-img1 = cv2.imread("cam_74.pgm", cv2.IMREAD_GRAYSCALE)
+if len(sys.argv) != 4:
+    print("The call to the function is incorrect, 3 elements need to be introduced ")
+    exit()
+
+img1 = cv2.imread(sys.argv[1], cv2.IMREAD_GRAYSCALE)
+
+value = int(sys.argv[2])
 
 # Copy of image
 img2 = img1.copy()
 
-nrows = img1.shape[0]
-ncols = img1.shape[1]
-
+# Thresholding operation
 for row in range(img1.shape[0]):
     for colum in range (img1.shape[1]):
-        if img2[row,colum] >
+        if img2[row,colum] >= value:
+            img2[row,colum] = 255
+        else:
+            img2[row,colum] = 0
+
+# Save the result 
+cv2.imwrite(sys.argv[3], img2);
+
+print("Showing image in window... Press a key to finish.")
+cv2.imshow("Window: img2", img2)
+cv2.waitKey(0) # waiting for a key
---
+++ Ejercicios1/README.md
@@ -0,0 +1,9 @@
+## Exercise 2 
+Call exercise 2a: 
+    python3 exercise_02a_thresh.py exercise_02a_input_01.pgm value exercise_02a_output_01.pgm
+    example : python3 exercise_02a_thresh.py cam_74.pgm 100 cam_74_threshold100Exercise.pgm
+
+Call exercise 2b:
+    python3 exercise_02b_compare.py exercise_02b_input_01.pgm exercise_02b_input_02.pgm
+    example1 : python3 exercise_02b_compare.py cam_74.pgm cam_74_threshold100Exercise.pgm (the result is 0)
+    example2 : python3 exercise_02b_compare.py cam_74_threshold100.pgm cam_74_threshold100Exercise.pgm (the result is 1)
